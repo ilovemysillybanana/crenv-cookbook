24dbed5e55a93ab222a5fd38146cca0ced48057d
Jose M. Tobar <ilovemysillybanana@gmail.com>
2016-12-18 19:51:00 -0600

    Added to README

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# crenv
-
-TODO: Enter the cookbook description here.
-
 # crenv-cookbook
+A cookbook to install and configure CRENV, which is a tool to install and manage multiple sets of shards and crystal language versions.
+
+For more information regarding CRENV, please this [repository](https://github.com/pine/crenv).
+
+## Platforms
+* Ubuntu 12.04+
+* CentOS 6+
+* Debian 7+
+* Fedora 25
+
+All other Debian or RHEL linux distros are assumed to work. If you find an issue with any of them that aren't listed here, feel free to submit a pull request/issue.
+
+## Dependencies
+* Chef 12.5+
--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,11 @@
 name 'crenv'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'Jose M. Tobar'
+maintainer_email 'ilovemysillybanana@gmail.com'
+license 'Apache 2.0'
 description 'Installs/Configures crenv'
 long_description 'Installs/Configures crenv'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/crenv/issues' if respond_to?(:issues_url)
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/crenv' if respond_to?(:source_url)
+issues_url 'https://github.com/ilovemysillybanana/crenv-cookbook' if respond_to?(:issues_url)
+source_url 'https://github.com/ilovemysillybanana/crenv-cookbook/issues' if respond_to?(:source_url)
