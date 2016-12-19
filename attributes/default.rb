c467e69f615391b2bb835aa8af9079011892ddef
Jose M. Tobar <ilovemysillybanana@gmail.com>
2016-12-19 00:03:33 -0600

    Added default attributes

---
+++ attributes/default.rb
@@ -0,0 +1,27 @@
+#git repository for crenv
+default['crenv']['git_url'] = 'https://github.com/pine/crenv.git'
+default['crenv']['git_ref'] = 'v1.1.0'
+
+#custom repos for building and updating crystal
+default['crenv']['build_repo'] = 'https://github.com/pine/crystal-build.git'
+default['crenv']['update_repo'] = 'https://github.com/pine/crenv-update.git'
+
+#upgrade crenv
+default['crenv']['upgrade'] = 'none'
+
+#user hash, for each to have a rbenv installation
+default['crenv']['user_installs'] = []
+
+#extra system wide tunables
+default['crenv']['root_path'] = '/usr/local/crenv'
+
+#shards to install for versions/users
+default['crenv']['shards'] = {}
+default['crenv']['user_shards'] = {}
+
+# list of crenv plugins to install
+default['crenv']['plugins']      = []
+default['crenv']['user_plugins'] = []
+
+#create the user profile.d shell script
+default['crenv']['create_profiled'] = true
