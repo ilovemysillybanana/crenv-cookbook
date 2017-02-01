fff47789b9e17629714b25986cbe276f03f93a90
Jose M. Tobar <ilovemysillybanana@gmail.com>
2017-01-31 21:26:25 -0600

    Added ChefSpec Test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
+  config.log_level = :error
+
+  config.run_all_when_everything_filtered = true
+
+  Ohai::Config[:log_level] = :error
+end
