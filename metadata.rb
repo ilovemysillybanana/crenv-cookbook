d0ed8f6bb9a9943c49fcd6705da203b8c726dfc8
Jose M. Tobar <ilovemysillybanana@gmail.com>
2017-04-08 18:40:14 -0500

    Added supports operating systems

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,16 @@ depends 'sudo', '~> 3.0.0'
 depends 'git', '~> 5.0.0'
 depends 'build-essential', '~> 7.0.3'
 
+supports 'ubuntu', '= 14.04'
+supports 'ubuntu', '= 16.04'
+supports 'ubuntu', '= 16.10'
+supports 'debian', '~> 8.0.0'
+supports 'fedora', '~> 23.0.0'
+supports 'fedora', '~> 24.0.0'
+supports 'centos', '>= 7.1'
+supports 'oracle', '>= 7.1'
+supports 'redhat', '>= 7.1'
+
 
 source_url 'https://github.com/ilovemysillybanana/crenv-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/ilovemysillybanana/crenv-cookbook/issues' if respond_to?(:issues_url)
